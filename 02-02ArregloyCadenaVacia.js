749a97274ad2fe555f19d12d4495eca456e885cd
DUM <114031187+Danielauribe1@users.noreply.github.com>
2022-10-11 14:34:40 -0500

    Segundo avance segunda semana de octubre JS

---
+++ 02-02ArregloyCadenaVacia.js
@@ -0,0 +1,13 @@
+/*
+Ejemplo de un arreglo y de una cadena vacia
+*/
+
+//arreglo en JavaScript
+var autos = ['BMW','Audi','Mercedes'];
+console.log(autos);
+console.log(typeof autos);
+
+//Cadena vacia (empty string)
+var z = '';
+console.log(z);
+console.log(typeof z);
