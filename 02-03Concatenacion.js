a0985016e8610962103f7844bb63225d74a03129
DUM <114031187+Danielauribe1@users.noreply.github.com>
2022-10-13 15:17:55 -0500

    Tercer avance segunda semana de octubre JS

---
+++ 02-03Concatenacion.js
@@ -0,0 +1,17 @@
+var nombre = 'Daniela';
+var apellido = 'Uribe';
+
+var nombreCompleto = nombre + ' ' + apellido;
+console.log(nombreCompleto);
+
+var nombreCompleto2 = 'Dora' + ' ' + 'Mazo';
+console.log(nombreCompleto2);
+
+var x = nombre + 2 + 4;
+console.log(x);
+
+x = nombre + (2 + 4);
+console.log(x);
+
+x = 2 + 4 + nombre;
+console.log(x);
